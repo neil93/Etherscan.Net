b936b8db202e11c779795ee57ccb5ac77e783e87
neil <tasi.neil@gmail.com>
2025-11-13 17:31:14 +0800

    加入EOA 手續費Event

---
+++ EthScanNet.Lib/Models/Events/LogFeeTransfer.cs
@@ -0,0 +1,36 @@
+﻿using Nethereum.ABI.FunctionEncoding.Attributes;
+using System.Numerics;
+
+namespace EthScanNet.Lib.Models.Events
+{
+    /// <summary>
+    /// 交易手续费扣除事件
+    /// </summary>
+    [Event("LogFeeTransfer")]
+    public class LogFeeTransfer : IEventDTO
+    {
+        [Parameter("address", "token", 1, true)]
+        public string Token { get; set; } = string.Empty;
+
+        [Parameter("address", "from", 2, true)]
+        public string From { get; set; } = string.Empty;
+
+        [Parameter("address", "to", 3, true)]
+        public string To { get; set; } = string.Empty;
+
+        [Parameter("uint256", "amount", 4)]
+        public BigInteger Amount { get; set; }
+
+        [Parameter("uint256", "input1", 5)]
+        public BigInteger Input1 { get; set; }
+
+        [Parameter("uint256", "input2", 6)]
+        public BigInteger Input2 { get; set; }
+
+        [Parameter("uint256", "output1", 7)]
+        public BigInteger Output1 { get; set; }
+
+        [Parameter("uint256", "output2", 8)]
+        public BigInteger Output2 { get; set; }
+    }
+}
